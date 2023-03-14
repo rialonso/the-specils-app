ddb9b21a4871e25b60437f823f383a012ca1c897
Murilo Cardoso <muri.cardoso@hotmail.com>
2023-03-14 17:32:57 -0300

    ajustes de imagens, e consumo de api para sugestao de matchs

---
+++ lib/env/env.dart
@@ -0,0 +1,6 @@
+class _Env {
+  static const String baseURL = 'https://apiv2.devotee.com.br/';
+  static const String login = 'api/login';
+  static const String suggestionCards = 'api/cards';
+
+}
--- lib/screens/suggestion_matchs/suggestion_matchs.dart
+++ lib/screens/suggestion_matchs/suggestion_matchs.dart
@@ -1,23 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:the_specials_app/shared/blocs/login_bloc.dart';
+import 'package:the_specials_app/shared/blocs/suggestion_cards_bloc.dart';
+import 'package:the_specials_app/shared/components/suggestion_card.dart';
 import 'package:the_specials_app/shared/state_management/logged_user_data/logged_user_data.dart';
+import 'package:the_specials_app/shared/state_management/suggestion_cards/suggestion_cards.dart';
 import 'package:the_specials_app/shared/styles/buttons.dart';
 
 class SuggestionMatchs extends StatefulWidget {
-  const SuggestionMatchs({super.key, this.dataD});
-  final dynamic dataD;
+  const SuggestionMatchs({super.key});
   @override
   State<SuggestionMatchs> createState() => _SuggestionMatchs();
 }
 
 class _SuggestionMatchs extends State<SuggestionMatchs> {
-  final LoginBloc _loginBloc = LoginBloc();
+  dynamic suggestionCardsController = Get.put(SuggestionCardsController());
+  SuggestionCardsData? suggestionCardsData;
+  final SuggestionCardsBloc _bloc = SuggestionCardsBloc();
 
+  waitSuggestionCards() async {
+     await _bloc.getSuggestionCards();
+     suggestionCardsData = await suggestionCardsController.getSuggestionCards();
+  }
+
+  createSuggestionCards (dynamic suggestionData) {
+    // var textEditingControllers = <SuggestionCards>[];
+    List<Widget> suggestionCardsWidget = [];
+    // var list = List.generate(suggestionData.data, (i) =>i + 1 );
+    // print(list);
+    // print(suggestionData);
+    suggestionData.data.forEach((suggestionCardData) {
+      // print(i);
+      // var textEditingController = new SuggestionCards(suggestionCardsData: suggestionCardsData,);
+      // textEditingControllers.add(textEditingController);
+      return suggestionCardsWidget.add(SuggestionCards(suggestionCardsData: suggestionCardData));
+    });
+    print(suggestionCardsWidget);
+    return suggestionCardsWidget;
+  }
+
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => waitSuggestionCards());
+  }
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<LoggedUserDataController>(
-      init: LoggedUserDataController(),
+    return GetBuilder<SuggestionCardsController>(
+      init: SuggestionCardsController(),
       builder: (_) => Scaffold(
         appBar: PreferredSize(
             preferredSize: const Size.fromHeight(0.0),
@@ -30,9 +62,15 @@ class _SuggestionMatchs extends State<SuggestionMatchs> {
         body: Padding(
           padding: const EdgeInsets.all(25.0),
           child: Column(
-            children: [ButtonPrimary(onPressed: () {
-              _.getUserData();
-              print('d');
+            children: [
+              // SuggestionCards(suggestionCardsData: suggestionCardsData),
+              Stack(
+                 children:  createSuggestionCards(suggestionCardsData),
+              ),
+              ButtonPrimary(onPressed: () async {
+                // await waitSuggestionCards();
+                var d = await suggestionCardsController.getSuggestionCards();
+                print(d.toJson());
             }, texto: 'ss')],
           ),
         ),
